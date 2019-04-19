6e97ee31b8ac5ef486551cc4eb3fd58efc13096b
XieGuochao <117010307@link.cuhk.edu.cn>
2019-04-19 18:12:45 +0800

    admin

--- dorm/dormapp/admin.py
+++ dorm/dormapp/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from dormapp.models import Record
 # Register your models here.
 
-admin.site.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('college', 'note', 'IPadd', 'IPdelete')
+admin.site.register(Record, RecordAdmin)
