d09897f45901b742248b4579973fc632d95fc8c5
XieGuochao <117010307@link.cuhk.edu.cn>
2019-04-19 18:23:55 +0800

    static

--- dorm/dorm/urls.py
+++ dorm/dorm/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from dormapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('query/', views.query),
     path('delete/', views.delete),
     path("", views.index)
-]
+] + static(settings.STATIC_ROOT, document_root=settings.STATAIC_ROOT)
